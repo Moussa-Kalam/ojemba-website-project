edd9b0579dd58550903d1d62a661729b0fe2c645
danielerat <davidodo2015@gmail.com>
2023-08-16 17:47:12 +0200

    Adding predefined colors and backgrounds to be used in the entire site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./**/*.{html,js}"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: "rgba(0, 19, 66, 1)", //dark blue
+      secondary: "rgba(138, 204, 80, 1)", //main green
+      "secondary-dark": "rgba(17, 99, 85, 1)", //dark green
+      accent: "rgba(244, 186, 0, 1)", //Yellow
+    },
+    backgroundImage: {
+      "second-top":
+        "linear-gradient(180deg,rgba(138, 204, 80, 0.5), rgba(0, 0, 0, 0))",
+      "second-top-left":
+        "linear-gradient(135deg,rgba(138, 204, 80, 0.5), rgba(255, 0, 0, 0))",
+      "second-bottom-right":
+        "linear-gradient(-90deg,rgba(138, 204, 80, 0.5), rgba(255, 0, 0, 0))",
+      "second-left":
+        "linear-gradient(45deg,rgba(138, 204, 80, 0.5), rgba(0, 0, 0, 0))",
+      "second-right":
+        "linear-gradient(-45deg,rgba(138, 204, 80, 0.5), rgba(0, 0, 0, 0))",
+    },
+  },
 };
 export const plugins = [];
